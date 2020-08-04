5e01e7bea9caa176e5c28c36ee5bb0593571f2b6
Lazi <43828977+Lazi-M@users.noreply.github.com>
2020-08-04 19:17:14 +0200

    Create Week2Lect4Exe2.cs
    Napisz program, w którym stworzysz 3 zmienne z jedną literą, a następnie wypiszesz je w
    odwrotnej kolejności niż zostały zadeklarowane

---
+++ Week2Lect4Exe2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DotNetExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char d = 't', o = 'e', t = 'n';
+            Console.WriteLine($"{t}{o}{d}");
+        }
+    }
+}
