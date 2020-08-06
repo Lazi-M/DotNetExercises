590c904e7345fd9d06c5112e5e9106a3d936bfbd
Lazi <43828977+Lazi-M@users.noreply.github.com>
2020-08-06 19:55:01 +0200

    Add files via upload

---
+++ Week_2_Lect_4/Exe_1.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace DotNetExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string lastname;
+            int age;
+            char genderM = 'm';
+            char genderF = 'f';
+            long pesel;
+            long idWorker;    
+        }
+    }
+}
---
+++ Week_2_Lect_4/Exe_4.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DotNetExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            string schoolName = "Szkoła Dotneta";
+            double number2 = 12.5;
+        }
+    }
+}
